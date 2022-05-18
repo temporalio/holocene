d6ab83bc244a23e7f2c386cb4ef2eff79a5e6775
Alex Tideman <alex.tideman@gmail.com>
2022-05-18 10:15:16 -0500

    Add routes to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import Navigation from '$lib/components/navigation/index.svelte';
+  import { routes } from '$lib/routes';
   import '../app.css';
 </script>
 
 <div class="flex flex-row w-screen h-screen">
-  <div class="sticky top-0 h-screen bg-white"><Navigation /></div>
+  <div class="sticky top-0 h-screen bg-white">
+    <Navigation linkList={routes} />
+  </div>
   <div class="flex-auto h-screen w-max overflow-auto"><slot /></div>
 </div>
